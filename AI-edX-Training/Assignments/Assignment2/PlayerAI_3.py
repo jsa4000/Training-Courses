874d8fd6766cd25b5eb22aadf8726da092d26f21
jsa4000 <jsa4000@gmail.com>
2017-06-12 15:22:49 +0200

    modify some files

--- AI-edX-Training/Assignments/Assignment2/PlayerAI_3.py
+++ AI-edX-Training/Assignments/Assignment2/PlayerAI_3.py
@@ -3,114 +3,6 @@ import math
 from random import randint
 from BaseAI_3 import BaseAI
 
-directionVectors = (UP_VEC, DOWN_VEC, LEFT_VEC, RIGHT_VEC) = ((-1, 0), (1, 0), (0, -1), (0, 1))
-vecIndex = [UP, DOWN, LEFT, RIGHT] = range(4)
-
-def getHeuristicScore(self, newGrid, oldGrid):
-    
-    score = 0
-    
-    # (1): actual score
-    addScore = newGrid.score
-    if addScore > 0:
-        addScore = math.log(addScore) / math.log(2)
-        
-    # (2): empty fields
-    # the higher the score, the more important the empty fields
-    # highest score * available fields
-    empty = 0
-    if len(newGrid.getAvailableCells()) > 1:
-        empty = math.log(len(newGrid.getAvailableCells())) / math.log(2)
-    
-    # (3): how close are our numbers?
-    close = self.smoothness(newGrid) * math.log(newGrid.getHighestValue()) / math.log(2)
-    
-    # (4): are we close to a great corner order?
-    order = self.getOrderScore(newGrid)
-    
-    # (5): what is our highest number so far?
-    fields = len(newGrid.getAvailableCells())
-    emptyFields = 0
-    if not fields == 0 and not newGrid.getHighestValue() == 0:
-        emptyFields = math.log(newGrid.getHighestValue()) / math.log(2) + newGrid.getHighestValue()
-    
-        return close * 0.1 + emptyFields * 1 + order * 1.3 + empty * 2.7
-    return score
-
-def getOrderScore(self, grid):
-    
-    score = 0
-    
-    # since there are just 4 rows and 4 columns, I can score it statically
-    highestVal = grid.getHighestValue()
-    x = 0
-    y = 0
-    
-    # left up corner
-    if grid.map[0][0] == highestVal:
-        score += highestVal
-        x = 0
-        y = 0
-        
-    # left down corner
-    if grid.map[3][0] == highestVal:
-        score += highestVal
-        x = 3
-        y = 0
-       
-    # right up corner
-    if grid.map[0][3] == highestVal:
-        score += highestVal
-        x = 0
-        y = 3
-        
-    # right down corner
-    if grid.map[3][3] == highestVal:
-        score += highestVal
-        x = 3
-        y = 3
-        
-    return (math.log(score)) if score > 0 else 0
-
-def gridInsertOk(grid, cell):
-    return cell[0] >= 0 and cell[0] < len(grid.map) and cell[1] >= 0 and cell[1] < len(grid.map[0])
-
-def findFarthestAway(grid, cell, vector):
-    
-    previous = cell
-    cell = [previous[0] + vector[0], previous[1] + vector[1]]
-        
-    while gridInsertOk(grid, cell) and grid.map[cell[0]][cell[1]] == 0:
-        previous = cell
-        cell = [previous[0] + vector[0], previous[1] + vector[1]]
-        
-    if gridInsertOk(grid, cell) == False:
-        cell = previous
-            
-    return cell
-
-#MH3478 END
-
-
-def smoothness(grid):
-    smoothness = 0
-    for x in range (0, 4):
-        for y in range(0, 4):
-            if grid.map[x][y] != 0:
-                #SMOOTHNESS
-                value = math.log(grid.map[x][y]) / math.log(2)
-                vecRange = { 1, 0 , 2, 3 }
-                for dir in vecRange:
-                    vec = directionVectors[dir]
-                    targetCell = findFarthestAway(grid, [x, y], vec)
-                    targetValue = grid.map[targetCell[0]][targetCell[1]]
-                    if targetValue > 0:
-                        tValue = math.log(targetValue) / math.log(2)
-                        smoothness -= abs(value - tValue)
-                
-
-    return smoothness 
-
 def get_heuristic_score(grid):
     """ This function gets the current score for the current
     grid. The score is independt of the minmax node, since the
@@ -128,7 +20,7 @@ def get_heuristic_score(grid):
     """
     actual_score = grid.getMaxTile()
     available_cells = len(grid.getAvailableCells())
-    clustering_score = smoothness(grid)
+    clustering_score = 1.0
 
     # Compute the current scode
     score = int(actual_score+math.log(actual_score)*available_cells - clustering_score)
