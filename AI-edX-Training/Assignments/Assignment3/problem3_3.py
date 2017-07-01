a4367e6f893d7bd66f7170a97a71f12b7e78083c
Javier Santos <jsa4000@gmail.com>
2017-07-01 09:32:44 +0200

    Code organized

--- AI-edX-Training/Assignments/Assignment3/problem3_3.py
+++ AI-edX-Training/Assignments/Assignment3/problem3_3.py
@@ -27,11 +27,127 @@ from plots import *
  - Random Forest. (Similar to above).
   
 '''
+# Import Models from sklearn
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+
+# Cross validation with stratified sampling by default
+from sklearn.model_selection import cross_val_score
+
+def svm_linear(data,targets):
+    '''  SVM with Linear Kernel. 
+
+    Observe the performance of the SVM with linear kernel. Search for a 
+    good setting of parameters to obtain high classification accuracy. 
+    Specifically.
+    
+    try values of:
+        
+        C = [0.1, 0.5, 1, 5, 10, 50, 100]. 
+
+    Read about sklearn.grid_search and how this can help you accomplish 
+    this task. After locating the optimal parameter value by using the 
+    training data, record the corresponding best score (training data 
+    accuracy) achieved. 
+    
+    Then apply the testing data to the model, and 
+    record the actual test score. Both scores will be a number between 
+    zero and one.
+    '''
+
+    best_score = 0.0
+    test_score = 0.0
+
+    return best_score, test_score
+
+def svm_polynomial(data,targets):
+    '''SVM with Polynomial Kernel. (Similar to above).
+        
+    Try values of :
+
+        C = [0.1, 1, 3]
+        degree = [4, 5, 6] 
+        gamma = [0.1, 0.5]
+
+    '''
+    best_score = 0.0
+    test_score = 0.0
+
+    return best_score, test_score
+
+def svm_rbf(data,targets):
+    ''' SVM with RBF Kernel. (Similar to above).
+
+    Try values of:
+
+        C = [0.1, 0.5, 1, 5, 10, 50, 100] 
+        gamma = [0.1, 0.5, 1, 3, 6, 10]
+
+    '''
+    best_score = 0.0
+    test_score = 0.0
+
+    return best_score, test_score
+
+def logistic(data,targets):
+    ''' Logistic Regression. (Similar to above).
+
+    Try values of:
+    
+        C = [0.1, 0.5, 1, 5, 10, 50, 100]
+
+    '''
+    best_score = 0.0
+    test_score = 0.0
+
+    return best_score, test_score
+
+def knn(data,targets):
+    ''' k-Nearest Neighbors. (Similar to above).
+
+    Try values of:
+
+        n_neighbors = [1, 2, 3, ..., 50] 
+        leaf_size = [5, 10, 15, ..., 60]
+
+    '''
+    best_score = 0.0
+    test_score = 0.0
+
+    return best_score, test_score
+
+def decision_tree(data,targets):
+    ''' Decision Trees. (Similar to above).
+
+    Try values of:
+
+        max_depth = [1, 2, 3, ..., 50]
+        min_samples_split = [2, 3, 4, ..., 10]
+
+    '''
+    best_score = 0.0
+    test_score = 0.0
+
+    return best_score, test_score
+
+def random_forest(data,targets):
+    ''' Random Forest. (Similar to above).
+
+    Try values of:
+    
+         max_depth = [1, 2, 3, ..., 50] 
+         min_samples_split = [2, 3, 4, ..., 10]
+
+    '''
+    best_score = 0.0
+    test_score = 0.0
+
+    return best_score, test_score
+
+
 
 if __name__ == "__main__":
     # Start the Program 
@@ -52,9 +168,20 @@ if __name__ == "__main__":
     if test_mode: test_dataset(df) # If test mode enabled (2D)
 
     # Get training and labels from dataset
-    training_set = df.loc[:,["A","B"]]
-    labels = df.loc[:,"label"]
+    data = df.loc[:,["A","B"]]
+    targets = df.loc[:,"label"]
    
+    # Create models to use cross-validation
+    models = [svm_linear,svm_polynomial,svm_rbf,logistic,knn,decision_tree,random_forest]
+    scores =  []
+    for model in models:
+        scores.append(model(data,targets))
+
+    # Write current outputs into the file
+  # Write current weights into the file
+    with open(output_file,"w") as file:
+        for index, score in enumerate(scores):
+            file.write("{},{}\n".format(models[index].__name__,",".join(format(score_type, "0.8f") for score_type in score)))
 
     # End of the Program
 
